4e913c4c24fc51ba1cb6b8d36aa4a654aec79f78
Aniket Singh <31aniket.singh@gmail.com>
2023-01-01 17:56:14 +0530

    Initial Commit

---
+++ portfolio/portfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  portfolioApp.swift
+//  portfolio
+//
+//  Created by Aniket Singh on 01/01/23.
+//
+
+import SwiftUI
+
+@main
+struct portfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
